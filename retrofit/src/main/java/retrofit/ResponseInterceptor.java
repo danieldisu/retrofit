71e74c67a45815ca49757e73bd7e6fe7dc2722ab
Daniel Diaz Suarez <ddiaz@autentia.com>
2014-04-28 17:16:35 +0200

    Now you can Intercept Responses

---
+++ retrofit/src/main/java/retrofit/ResponseInterceptor.java
@@ -0,0 +1,92 @@
+package retrofit;
+
+import org.apache.commons.io.IOUtils;
+import retrofit.client.Header;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.mime.TypedByteArray;
+import retrofit.mime.TypedInput;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * Created by ddiaz on 25/04/14.
+ */
+public interface ResponseInterceptor {
+  ResponseFacade intercept(ResponseFacade responseFacade);
+
+  boolean shouldRepeatRequestAfterIntercept();
+
+  public class ResponseFacade {
+
+    private final Response originalResponse;
+    private Response newResponse;
+    private TypedByteArray originalResponseBody = null;
+
+    ResponseFacade(Response originalResponse) {
+      this.originalResponse = originalResponse;
+      this.newResponse = new Response(originalResponse.getUrl(), originalResponse.getStatus(), originalResponse.getReason(), originalResponse.getHeaders(), getOriginalResponseBody());
+    }
+
+    private void changeResponseBody(String newBody) {
+      // TODO
+    }
+
+    public void changeStatus(int newStatus) {
+      this.newResponse = new Response(newResponse.getUrl(), newStatus, newResponse.getReason(), newResponse.getHeaders(), newResponse.getBody());
+    }
+
+    public void changeHeaders(List<Header> headers) {
+      this.newResponse = new Response(newResponse.getUrl(), newResponse.getStatus(), newResponse.getReason(), headers, newResponse.getBody());
+    }
+
+    public void addHeader(Header header) {
+      originalResponse.getHeaders().add(header);
+      this.newResponse = new Response(newResponse.getUrl(), newResponse.getStatus(), newResponse.getReason(), newResponse.getHeaders(), newResponse.getBody());
+    }
+
+    public TypedByteArray getOriginalResponseBody() {
+      if(originalResponseBody != null){
+        return originalResponseBody;
+      }
+
+      TypedByteArray newBody = null;
+      InputStream is = null;
+      try {
+        is = originalResponse.getBody().in();
+        byte[] bytes = IOUtils.toByteArray(is);
+        originalResponseBody = new TypedByteArray(originalResponse.getBody().mimeType(), bytes);
+        return originalResponseBody;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return originalResponseBody;
+    }
+
+    public int getOriginalResponseStatus() {
+      return originalResponse.getStatus();
+    }
+
+    protected Response getNewResponse() {
+      return this.newResponse;
+    }
+  }
+
+  /**
+   * A {@link ResponseInterceptor} which does no modification of requests.
+   */
+  ResponseInterceptor NONE = new ResponseInterceptor() {
+    @Override
+    public ResponseFacade intercept(ResponseFacade responseFacade) {
+      return responseFacade;
+    }
+
+    @Override public boolean shouldRepeatRequestAfterIntercept() {
+      return false;
+    }
+  };
+}
